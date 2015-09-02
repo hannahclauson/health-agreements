fe85b5ee22028def55375e2b7cb342156b40a7f5
Sean Jezewski <seanwjezewski@gmail.com>
2015-09-02 16:55:51 -0700

    Add url validator

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ActiveRecord::Base
   validates :name, presence: true, length: {minimum: 3}
-  
+  validates :url, presence: true
+  validates :url, format: {with: URI.regexp }, if: Proc.new {|a| a.url.present?}
 end
