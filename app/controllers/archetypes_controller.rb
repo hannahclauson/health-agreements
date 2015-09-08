c55155317957060ca6a80b0ee01a132675519d51
Sean Jezewski <seanwjezewski@gmail.com>
2015-09-07 18:55:31 -0700

    Fill in new / create / _form views for archetypes

--- app/controllers/archetypes_controller.rb
+++ app/controllers/archetypes_controller.rb
@@ -4,6 +4,28 @@ class ArchetypesController < ApplicationController
   end
 
   def new
-    
+    @archetype = Archetype.new
   end
+
+  def show
+    @archetype = Archetype.find(params[:id])
+    @practice = Practice.new
+  end
+
+  def create
+    @archetype = Archetype.new(allowed_params)
+
+    if @archetype.save
+      redirect_to @archetype
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.require(:archetype).permit(:name, :description)
+  end
+
 end
--- app/models/archetype.rb
+++ app/models/archetype.rb
@@ -1,2 +1,6 @@
 class Archetype < ActiveRecord::Base
+  has_many :practices, dependent: :destroy
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
