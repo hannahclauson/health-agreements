f26697435a43ee303a195d1f6cb8fe74f4dfaaf0
Sean Jezewski <seanwjezewski@gmail.com>
2015-09-14 11:42:17 -0700

    Configure mailer for dev. Still not getting emails though. For now will just get conf links by looking at the logs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,16 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => ENV["GMAIL_ADDRESS"],
+    :password => ENV["GMAIL_PASSWORD"]
+  }
 end
