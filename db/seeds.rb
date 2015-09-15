473556e9c34cf392312f6d98ba2b62e69d00c378
Sean Jezewski <seanwjezewski@gmail.com>
2015-09-14 17:50:12 -0700

    Dont auto confirm, let the mailer do the work

--- db/seeds.rb
+++ db/seeds.rb
@@ -86,7 +86,6 @@ admin = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do
   user.password = Rails.application.secrets.admin_password
   user.password_confirmation = Rails.application.secrets.admin_password
   user.admin!
-  user.confirm
 end
 
 puts "Created admin : #{admin.email}"
