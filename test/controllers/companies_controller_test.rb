c6a52bc891205bd884bd581a48e096b59cd05be7
Sean Jezewski <seanwjezewski@gmail.com>
2015-09-17 19:05:34 -0700

    Adding in example access test for company creation - positive control for editors who should be allowed negative control for anon users who shouldnt be allowed Probably a better way to abstract this. But for now it works

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -11,10 +11,39 @@ class CompaniesControllerTest < ActionController::TestCase
     @admin.confirm
   end
 
+  # Public Access Tests
+
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:companies)
+  end
+
+  test "should not create" do
+    request.env["HTTP_REFERER"] = companies_path
+    post :create, company: {name: 'acme', url: 'http://acme.com', description: 'sells everything'}
+    assert_redirected_to companies_path
+  end
+
+  test "should show" do
+    get :show, :id => @company.id
+    assert_response :success
+    assert_not_nil assigns(:company)
+  end
+
+  # Editor Tests
+
+  test "should get new" do
+    sign_in @editor
+    get :new
+    assert_response :success
+  end
+
+  test "should create" do
+    sign_in @editor
+    post :create, company: {name: 'acme', url: 'http://acme.com', description: 'sells everything'}
+    assert_not_nil @response.headers['Location']
+    assert_redirected_to company_path(assigns[:company].id)
   end
 
   test "should get edit" do
@@ -22,5 +51,9 @@ class CompaniesControllerTest < ActionController::TestCase
     get :edit, id: @company.id
     assert_response :success
   end
+
+
+
+
 
 end
