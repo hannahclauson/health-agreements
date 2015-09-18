ac4997a1f1da8a55ce4ffec12913438974151dbb
Sean Jezewski <seanwjezewski@gmail.com>
2015-09-17 18:32:14 -0700

    Setting up users for testing. Need to make sure to confirm the users so that the signin helper works properly

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -3,14 +3,29 @@ require 'test_helper'
 class CompaniesControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
+  setup do
+    @company = companies('23andme')
+    @editor = users(:emily_editor)
+    @editor.confirm!
+    @admin = users(:amon_admin)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:companies)
   end
 
-  test "should get show" do
-    
+  test "should get edit" do
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in @editor
+    get :edit, id: @company.id
+    puts "request"
+    puts @request.inspect
+    puts "response"
+    puts @response.inspect
+
+    assert_response :success
   end
 
 end
