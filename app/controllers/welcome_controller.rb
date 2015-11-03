845477a7d7d9ce4654d3a8cc89c8680cd77c9e03
Sean Jezewski <seanwjezewski@gmail.com>
2015-11-03 12:18:20 -0800

    Test passing. Content in place for first big section. Now styling time

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
     @badges = {}
     ["Research", "Single State Hosted", "HIPAA Compliance"].each do |name|
       @badges[name] = Badge.where(name: name).first
+      puts "#{name} nil? #{@badges[name].nil?}"
     end
 
   end
