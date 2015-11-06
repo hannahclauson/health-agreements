db56b271ba81f04426d82dc6fd5d4a1fbc0b9d0c
Sean Jezewski <seanwjezewski@gmail.com>
2015-11-05 17:36:52 -0800

    Set ID every time

--- build/staging.sh
+++ build/staging.sh
@@ -2,7 +2,10 @@
 git push git@heroku.com:symbolofhealth-staging.git $CIRCLE_SHA1:refs/heads/master
 heroku run rake db:migrate --app symbolofhealth-staging
 export VER=release-`cat build/version`.$CIRCLE_BUILD_NUM
+git config --global user.email "symbolofhealthbot@gmail.com"
+git config --global user.name "symbolofhealthbot"
 git tag -a $VER -m $VER
+git tag
 git push --tags
 #git@github.com:sjezewski/health-agreements.git
 #git fetch --tags
