d2a3fbd9255fb59d12d83a1d55113db9fb6b77e1
Sean Jezewski <seanwjezewski@gmail.com>
2015-11-05 17:08:25 -0800

    Another attempt. Only the tagging push seems to be failing. MAybe not specifying remote will be allowed

--- build/staging.sh
+++ build/staging.sh
@@ -3,5 +3,6 @@ git push git@heroku.com:symbolofhealth-staging.git $CIRCLE_SHA1:refs/heads/maste
 heroku run rake db:migrate --app symbolofhealth-staging
 export VER=release-`cat build/version`.$CIRCLE_BUILD_NUM
 git tag -a $VER -m $VER
-git push --tags git@github.com:sjezewski/health-agreements.git
+git push --tags
+#git@github.com:sjezewski/health-agreements.git
 #git fetch --tags
