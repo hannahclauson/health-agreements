2761224990487495009dc63dfc5f1fd2aba6830f
Sean Jezewski <seanwjezewski@gmail.com>
2015-11-06 11:46:25 -0800

    OK. Made search_param generation into a helper. Now need to update all the links on the homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -35,4 +35,5 @@ class WelcomeController < ApplicationController
     }
 
   end
+
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,23 @@
 module WelcomeHelper
+  def search_params(new_param)
+    # Only used on homepage to generate search links
+    # - its this or do redundant param walking/checking to see if things exist
+    # when I'm populating the search form after a search
+    {
+      :company => {
+        :name => ""
+      },
+      :badge => {
+        :id => "",
+        :name => ""
+      },
+      :guideline => {
+        :id => ""
+      },
+      :practice => {
+        :implementation => ""
+      },
+      :commit => "Search"
+    }.merge(new_param)
+  end
 end
