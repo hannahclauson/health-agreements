2faa0d08481a94a3f6c7a55a76e1f1df23e0459b
Sean Jezewski <seanwjezewski@gmail.com>
2015-11-12 08:52:20 -0800

    Im not seeing the effects of the migration on stage/prod, but I think thats because I havent dropped the DB there, so it thinks its already run the migration. So, made a new mig w the changes I need. Hopefully this time it runs

---
+++ db/migrate/20151112165020_contributions_and_legal_docs.rb
@@ -0,0 +1,27 @@
+class ContributionsAndLegalDocs < ActiveRecord::Migration
+  def change
+    create_table "legal_documents", force: :cascade do |t|
+      t.string   "name"
+      t.string   "url"
+      t.integer  "company_id"
+      t.datetime "created_at",  null: false
+      t.datetime "updated_at",  null: false
+    end
+
+    add_index "legal_documents", ["company_id"], name: "index_legal_documents_on_company_id", using: :btree
+
+    # Update practices schema
+
+    add_column "practices", "legal_document_id", :integer
+    add_index "practices", ["legal_document_id"], name: "index_practices_on_legal_document_id", using: :btree
+
+    create_table "contributions", force: :cascade do |t|
+      t.string   "kind"
+      t.string   "url"
+      t.string   "email"
+      t.datetime "created_at",  null: false
+      t.datetime "updated_at",  null: false
+    end
+
+  end
+end
