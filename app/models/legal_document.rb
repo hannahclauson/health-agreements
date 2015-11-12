7df085e35d56a1fdd8f95f6c62286f2056366202
Sean Jezewski <seanwjezewski@gmail.com>
2015-11-12 13:00:51 -0800

    Remove uniqueness criteria on legal docs --- many many comps will have ToS

--- app/models/legal_document.rb
+++ app/models/legal_document.rb
@@ -2,8 +2,8 @@ class LegalDocument < ActiveRecord::Base
 
   belongs_to :company
 
-  validates :name, presence: true, length: {minimum: 3}, uniqueness: true
-  validates :url, presence: true, uniqueness: true
+  validates :name, presence: true, length: {minimum: 3}
+  validates :url, presence: true
   validates :url, format: {with: URI.regexp }, if: Proc.new {|a| a.url.present?}
 
 end
